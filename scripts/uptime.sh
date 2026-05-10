67dbd4a3e87cdb5fcafe22e592dbde52a8535c7c
Nils Ratusznik <codeberg@gruik.net>
2026-05-10 15:59:25 +0200

    Have a more robust uptime.sh script.

--- scripts/uptime.sh
+++ scripts/uptime.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-uptime|awk '{print $(NF-2)" "$(NF-1)" "$NF}'
+
+if [ -f /proc/loadavg ]; then
+  # Linux
+  cut -d' ' -f1-3 /proc/loadavg
+elif command -v sysctl >/dev/null 2>&1; then
+  # BSD / macOS
+  sysctl -n vm.loadavg | tr -d '{}' | awk '{print $1, $2, $3}'
+else
+  # Fallback to anchored parsing
+  uptime | sed 's/.*load average[s]*: //' | tr -d ','
+fi
